1e87897363520efe2fa9f2cfdc4426bada2049e3
h8570rg <h8570rg@gmail.com>
2025-07-27 02:00:06 +0900

    feat: insatll mantine

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,15 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import FirestoreTest from "@/components/FirestoreTest";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        
-        <div>
-          <h1>Firebase Firestore テスト</h1>
-          <p>以下でFirestoreの動作をテストできます</p>
-        </div>
-        
-        {/* Firestoreテストコンポーネント */}
-        <FirestoreTest />
+    <div>
+      <div>
+        <h1>Firebase Firestore テスト</h1>
+        <p>以下でFirestoreの動作をテストできます</p>
+      </div>
 
-      </main>
+      {/* Firestoreテストコンポーネント */}
+      <FirestoreTest />
     </div>
   );
 }
